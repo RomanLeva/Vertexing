9be8cb095d2764b2aef0e069e3cf1745d83517bc
Roman <romanleva87@gmail.com>
2023-10-13 23:25:47 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/roma/Vertexing
-# Build directory: /home/roma/Vertexing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
